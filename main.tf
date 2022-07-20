39519c70c32ad9497f8cd88f3dc69030d83931da
Dustin Fuchs <84755626+vfuchs23@users.noreply.github.com>
2022-07-20 10:14:19 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "samcartest"
+ 
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "samcartest"
+    }
+  }
+}
